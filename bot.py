94747e568a01d38a03ff3bedb893e7904e4a8ea9
umapradeepan <umapradeepan@gmail.com>
2023-07-27 16:58:48 -0700

    Bot code missing trading broker API calls only

--- bot.py
+++ bot.py
@@ -1,4 +1,51 @@
-# init logger
+# encoding: utf-8
+
+# import libraries
+import sys
+from traderlib import *
+from logger import *
+
 # check trading account
-# ensure no orders are open
+def check_account_ok():
+    try:
+        account_info = None  # get account info TODO
+    except Exception as e:
+        lg.error("Could not get account info")
+        lg.info(str(e))
+        sys.exit()
+
+
+# close current orders
+def clean_open_orders():
+    open_orders = []  # get list of open orders TODO
+    lg.info("List of open orders")
+    lg.info(str(clean_open_orders))
+
+    for order in open_orders:
+        # close order TODO
+        lg.info("Order %s closed", str(order.id))
+
+    lg.info("Closing orders complete")
+
+
 # execute trading bot
+def main():
+    # init logger
+    init_logger()
+
+    # check our trading account
+    check_account_ok()
+
+    # close current orders
+    clean_open_orders()
+
+    # get ticker
+    ticker = input("Which ticker would you like to use?")
+
+    # run trading bot
+    trader = Trader()
+    trader.run()
+
+
+if __name__ == "__main__":
+    main()

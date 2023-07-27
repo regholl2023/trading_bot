bfeff29e4790ca11955eba7ac7030193655533e0
umapradeepan <umapradeepan@gmail.com>
2023-07-27 08:59:34 -0700

    Define overall structure for trading bot

---
+++ trading_bot.py
@@ -0,0 +1,28 @@
+# define asset
+
+# INITIAL CHECK
+# check position
+# check if tradable
+
+
+
+# LOOP - monitor market until buying a stock
+# load 30 min candles
+# perform general trend analysis
+# load 5 min candles
+# perform instant trend analysis
+# perform RSI analysis
+# perform stochastic analysis
+
+# SUBMIT ORDER - buy stock
+# submit order
+# check position
+
+# LOOP - monitor market until selling the stock
+# check take profit
+# check stop loss
+# check stochastic crossing
+
+# GET OUT - sell stock
+# check order
+# check position

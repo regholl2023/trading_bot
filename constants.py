5e39b0ee970b6270dbc97fdaebab03bdad9eda0f
umapradeepan <umapradeepan@gmail.com>
2023-07-27 16:58:48 -0700

    Bot code missing trading broker API calls only

---
+++ constants.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+# equity to spend per operation
+maxSpendEquity = 1000
+
+# boundaries for trading
+stopLossMargin = 0.05
+takeProfitMargin=0.1
+
+# max attempts section
+maxAttemptsCheckPosition = 5
+maxAttemptsCurrentPrice = 5
+maxAttemptsGeneralTrend = 10
+
+# sleep times section (seconds)
+sleepTimeCheckPosition = 5
+sleepTimeCurrentPrice = 5
+sleepTimeGeneralTrend = 600
--- logger.py
+++ logger.py
@@ -1,8 +1,36 @@
-import logging
-logging.basicConfig(filename='logging/log.log', format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
+import logging as lg
+import os
+from datetime import datetime
 
-# 
-logging.debug('This is a debugging message')
-logging.info('This is a info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
+"""
+    After init_logger() is called, logs will be written to 'logs/\{start time\}'.
+    Logs can be written by importing logging and writing messages to 4 different levels:
+        1. debug
+        2. info
+        3. warning
+        4. error
+    Logs will also be output to stdout.
+"""
+def init_logger():
+    # create folder for logs
+    logs_folder_path = './logs/'
+    try:
+        os.mkdir(logs_folder_path)
+    except OSError:
+        print("Creation of log directory %s failed", logs_folder_path)
+    else:
+        print("Successfully created log directory")
+        
+    # log parameters
+    current_time = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    current_log_path = logs_folder_path + current_time + '.log'
+    lg.basicConfig(filename=current_log_path, format='%(asctime)s - %(levelname)s: %(message)s', level=lg.DEBUG)
+    lg.getLogger().addHandler(lg.StreamHandler())
+    
+    # sample log
+    lg.info('Initialized logger')
+    
+# logging.debug('This is a debugging message')
+# logging.info('This is a info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
